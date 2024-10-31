9b86c5f22065ff3f7bac403099104a19f6afefd4
Anish <anishmu302@gmail.com>
2024-10-31 23:37:18 +0530

    Cart api creation

---
+++ src/main/java/pros/ElectronicStore/controller/CartController.java
@@ -0,0 +1,47 @@
+package pros.ElectronicStore.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pros.ElectronicStore.dtos.ApiResponseMessage;
+import pros.ElectronicStore.dtos.CartDto;
+import pros.ElectronicStore.helper.addedNewItemsDetails;
+import pros.ElectronicStore.services.CartService;
+
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+
+    @Autowired
+    CartService  cartService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<CartDto> addItems(@RequestBody addedNewItemsDetails itemsDetails, @PathVariable ("userId") String userId){
+        CartDto cartDto = cartService.addedNewItemsToCart(userId, itemsDetails);
+        return new ResponseEntity<>(cartDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<CartDto> getUserCart(@PathVariable ("userId") String userId){
+        CartDto cartByUser = cartService.getCartByUser(userId);
+        return  new ResponseEntity<>(cartByUser,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/items/{userId}/{itemId}")
+    public ResponseEntity<ApiResponseMessage> remove(@PathVariable("itemId") int id,@PathVariable ("userId") String userId){
+        cartService.remove(userId,id);
+        ApiResponseMessage itemRemovedFromCart = ApiResponseMessage.builder().message("item removed from cart").success(true).status(HttpStatus.OK).build();
+      return  new ResponseEntity<>(itemRemovedFromCart,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/items/{userId}")
+    public ResponseEntity<ApiResponseMessage> clearCart(@PathVariable ("userId") String userId){
+        cartService.clearCart(userId);
+        ApiResponseMessage itemRemovedFromCart = ApiResponseMessage.builder().message("Now cart is blank").success(true).status(HttpStatus.OK).build();
+        return  new ResponseEntity<>(itemRemovedFromCart,HttpStatus.OK);
+    }
+
+
+}
