d0a31c9951bce90b889d53bbf0dd5af51a77a981
Anish <anishmu302@gmail.com>
2024-11-08 22:52:37 +0530

    Basic auth implemenatation

--- src/main/java/pros/ElectronicStore/projectConfig/SecurityConfig.java
+++ src/main/java/pros/ElectronicStore/projectConfig/SecurityConfig.java
@@ -1,15 +1,19 @@
 package pros.ElectronicStore.projectConfig;
 
+import jakarta.servlet.http.HttpFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 public class SecurityConfig {
 
@@ -33,6 +37,22 @@ public class SecurityConfig {
 //
 //        return new InMemoryUserDetailsManager( user1,  user2);
 //    }
+    // below show the basic implementation of the Basic Auth
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .cors().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider(){
 
