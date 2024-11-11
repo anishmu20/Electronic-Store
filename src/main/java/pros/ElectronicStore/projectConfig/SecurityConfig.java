0ef99e61b61de481195052883e03421eb384a86e
Anish <anishmu302@gmail.com>
2024-11-12 00:48:27 +0530

    updating SecurityConfig file

--- src/main/java/pros/ElectronicStore/projectConfig/SecurityConfig.java
+++ src/main/java/pros/ElectronicStore/projectConfig/SecurityConfig.java
@@ -9,12 +9,14 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity(debug = true)
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
@@ -31,15 +33,15 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests(
                 request->{
                     request
+
                             .requestMatchers(HttpMethod.DELETE,"/users/**").hasRole("ADMIN")
                             .requestMatchers(HttpMethod.PUT,"/users/**").hasAnyRole("ADMIN","NORMAL")
                             .requestMatchers(HttpMethod.GET,"/products/**").permitAll()
                             .requestMatchers("/products/**").hasRole("ADMIN")
                             .requestMatchers(HttpMethod.GET,"/users/**").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/users").permitAll()
                             .requestMatchers(HttpMethod.GET,"/categories/**").permitAll()
                             .requestMatchers("/categories/**").hasRole("ADMIN");
-
-
                 }
                 );
 
