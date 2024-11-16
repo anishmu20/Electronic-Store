9aea3ec366f2d6d5835bd84d75303c9d98dc0ebe
Anish <anishmu302@gmail.com>
2024-11-16 21:59:37 +0530

    Auth controller

--- src/main/java/pros/ElectronicStore/controller/AuthController.java
+++ src/main/java/pros/ElectronicStore/controller/AuthController.java
@@ -2,16 +2,23 @@ package pros.ElectronicStore.controller;
 
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pros.ElectronicStore.dtos.JWTRequest;
+import pros.ElectronicStore.dtos.JWTResponse;
 import pros.ElectronicStore.dtos.UserDto;
-
-import java.security.Principal;
+import pros.ElectronicStore.entities.User;
+import pros.ElectronicStore.security.JwtHelper;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,11 +28,36 @@ public class AuthController {
     private UserDetailsService userDetailsService;
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    @Autowired
     private ModelMapper mapper;
 
-    @GetMapping("/current")
-    public ResponseEntity<UserDto> getUsername(Principal principal){
-        String name = principal.getName();
-        return new ResponseEntity<>(mapper.map(userDetailsService.loadUserByUsername(name), UserDto.class), HttpStatus.OK);
+    private final Logger logger= LoggerFactory.getLogger(AuthController.class);
+
+    @PostMapping("/generate-token")
+    public ResponseEntity<JWTResponse> login(@RequestBody JWTRequest request){
+        logger.info("Username {} ,Password {}",request.getEmail(),request.getPassword());
+        this.doAuthenticate(request.getEmail(),request.getPassword());
+        User user = (User) userDetailsService.loadUserByUsername(request.getEmail());
+        logger.info(user.toString());
+        String token= jwtHelper.generateToken(user);
+        JWTResponse response = JWTResponse.builder().token(token).user(mapper.map(user, UserDto.class)).build();
+        return ResponseEntity.ok(response);
+    }
+
+    private void doAuthenticate(String email, String password) {
+
+        try{
+            Authentication authentication=new UsernamePasswordAuthenticationToken(email,password);
+            logger.info(authentication.toString());
+            authenticationManager.authenticate(authentication);
+
+        }catch (BadCredentialsException e){
+            throw new BadCredentialsException("Invalid email and password");
+        }
     }
 }
