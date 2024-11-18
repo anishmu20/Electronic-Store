4019a0991f6b86b00bae3a1a00074f0c4fb13223
Anish <anishmu302@gmail.com>
2024-11-18 21:39:32 +0530

    creating constants

---
+++ src/main/java/pros/ElectronicStore/projectConfig/AppConstants.java
@@ -0,0 +1,7 @@
+package pros.ElectronicStore.projectConfig;
+
+public class AppConstants {
+
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_NORMAL = "NORMAL";
+}
