86b4fe27289b6ec78b4f5bedc66580b2896841d4
Anish <anishmu302@gmail.com>
2024-11-20 21:26:18 +0530

    Auth controller config to google user and normal user

--- src/main/java/pros/ElectronicStore/controller/AuthController.java
+++ src/main/java/pros/ElectronicStore/controller/AuthController.java
@@ -1,25 +1,37 @@
 package pros.ElectronicStore.controller;
 
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.apache.v2.ApacheHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 import pros.ElectronicStore.dtos.GoogleLogin;
 import pros.ElectronicStore.dtos.JWTRequest;
 import pros.ElectronicStore.dtos.JWTResponse;
 import pros.ElectronicStore.dtos.UserDto;
+import pros.ElectronicStore.entities.Providers;
 import pros.ElectronicStore.entities.User;
+import pros.ElectronicStore.exceptions.BadApiRequestException;
+import pros.ElectronicStore.exceptions.ResourceNotFound;
 import pros.ElectronicStore.security.JwtHelper;
+import pros.ElectronicStore.services.UserService;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.IllegalFormatCodePointException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
@@ -36,6 +48,15 @@ public class AuthController {
 
     @Autowired
     private ModelMapper mapper;
+
+    @Value("${google.client.id}")
+    private String googleClientApi;
+
+    @Value("${google.user.default.password}")
+    private  String googleDefaultPassword;
+
+    @Autowired
+    private UserService userService;
 
     private final Logger logger= LoggerFactory.getLogger(AuthController.class);
 
@@ -63,9 +84,65 @@ public class AuthController {
     }
 
     @PostMapping("/login-with-google")
-    public ResponseEntity<JWTResponse>LoginWithGoogle(@RequestBody GoogleLogin request ){
+    public ResponseEntity<JWTResponse>LoginWithGoogle(@RequestBody GoogleLogin request ) throws GeneralSecurityException, IOException {
         logger.info("Google Token : {}",request.getGoogleToken());
-        return null;
+        // token Verification
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new ApacheHttpTransport(), new GsonFactory()).setAudience(List.of(googleClientApi)).build();
+
+        GoogleIdToken idToken = verifier.verify(request.getGoogleToken());
+        if (idToken!=null){
+            GoogleIdToken.Payload payload = idToken.getPayload();
+            String email = payload.getEmail();
+            String  name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+            logger.info("Email {}",email);
+            logger.info("Name {}",name);
+            logger.info("familyName {}",familyName);
+            logger.info("pictureUrl {}",pictureUrl);
+            logger.info(" Given name {}",givenName);
+        // creating the user
+
+            UserDto userDto=new UserDto();
+            userDto.setName(name);
+            userDto.setImageName(pictureUrl);
+            userDto.setAbout("User created using a Google");
+            userDto.setEmail(email);
+            userDto.setPassword(googleDefaultPassword);
+            userDto.setProviders(Providers.GOOGLE);
+            UserDto userDto1=null;
+            try{
+                 userDto1 = userService.getUserByEmail(userDto.getEmail());
+                 logger.info("this time user is loaded from database ");
+
+                if (userDto1.getProviders().equals(userDto.getProviders())){
+                    // continue
+                }
+                else{
+                    throw new BadCredentialsException("Your email is Registered Through other provider try to login with ur Email and password");
+                }
+
+
+            }catch (ResourceNotFound ex){
+                logger.info("This time user is created  ");
+                userDto1 = userService.createUser(userDto);
+            }
+
+            this.doAuthenticate(userDto1.getEmail(),userDto.getPassword());
+            User user = mapper.map(userDto1, User.class);
+            String token= jwtHelper.generateToken(user);
+            JWTResponse response = JWTResponse.builder().token(token).user(mapper.map(user, UserDto.class)).build();
+            return ResponseEntity.ok(response);
+        }
+        else{
+            logger.info("Token is Invalid !!");
+            throw new BadApiRequestException("Invalid Google user !!");
+        }
+
+
+
     }
 
 }
